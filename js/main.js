bf530517eb4c0673eaf236b70b85d2846b982f3a
Jayson-Manas <jaysonmanas@gmail.com>
2018-11-07 14:44:10 +0200

    js execise

---
+++ js/main.js
@@ -0,0 +1,51 @@
+'use strict' 
+// use strict enable switching to the new modern mode. if not used strict program would be in the default mode. 
+
+// ===================================variables========================
+
+// used to store data or information
+// to create variables we use var or let
+
+
+// Example
+var message
+
+// assignment operator = (not math operator)
+
+// ==========================string========================================
+// set of characters surrounded by quotation marks
+// Example
+
+var message= "hello world" ;
+
+// alert(message);
+
+var name='nate';
+var age=43;
+var gender='female'
+
+console.log(name);
+console.log(age);
+console.log(gender);
+// note: always remember to close string with the same quotation marks
+
+// operators: + - * /
+
+// the + operator is used for concatination
+
+// var name = prompt('enter your first name:');
+
+// choose meaningful names
+// do not used reserved words
+// follow conventions
+
+// execise
+// My name is jayson. I am 23 years old. my favourite food is nandoos. I live at 26 tritonia street lentegeur mitchells plain
+// possible variables
+var name = 'jayson', age = 23,  favFood= 'nandoos', address='26 tritonia street lentegeur mitchells plain';
+
+
+console.log('My name is '+name+ '.I am ' +age+ ' years old. my favourite food is '+ favFood+ '. I live at '+ address);
+
+// =====================conditional statements================================
+// condition: 
